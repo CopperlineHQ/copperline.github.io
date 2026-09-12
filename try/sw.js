b1b9ecbfd77279392fca04e355810358410fec61
Andrew Hutchings <andrew@wolfssl.com>
2026-09-12 11:38:18 +0100

    Match the theme toggle icons to the documentation

--- try/sw.js
+++ try/sw.js
@@ -12,7 +12,7 @@
 // no version dance beyond bumping CACHE when the precache list changes
 // shape.
 
-const CACHE = 'copperline-try-v0.20.0-site-1';
+const CACHE = 'copperline-try-v0.20.0-site-2';
 
 const PRECACHE = [
   './',
